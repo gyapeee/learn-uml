eeb4163680be8f2ef29fe723ca5ba413a15accf0
gyapeee <gyapeee@gmail.com>
2021-12-07 01:48:49 +0100

    useless imports are removed

--- src/main/java/com/gyapeee/learnuml/PageObjectModelTest.java
+++ src/main/java/com/gyapeee/learnuml/PageObjectModelTest.java
@@ -1,8 +1,9 @@
 package com.gyapeee.learnuml;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.*;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
