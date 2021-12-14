0e74a431d4ede508dd865690b54d4f901dc25955
gyapeee <gyapeee@gmail.com>
2021-12-14 23:17:47 +0100

    prism.js added. code highlighting enabled.

--- src/main/java/com/gyapeee/learnuml/controller/AppController.java
+++ src/main/java/com/gyapeee/learnuml/controller/AppController.java
@@ -12,9 +12,32 @@ public class AppController {
     @Autowired
     private UserRepository userRepository;
 
+    private static final String codeBlock = "/**\n" +
+            "* Copy reference when passing-by-value\n" +
+            "*/\n" +
+            "class Apple {\n" +
+            "    public String color = \"red\";\n" +
+            "}\n" +
+            "\n" +
+            "public class Main {\n" +
+            "    public static void main(String[] args) {\n" +
+            "        // the Original reference\n" +
+            "        Apple apple = new Apple();\n" +
+            "        System.out.println(apple.color);\n" +
+            "\n" +
+            "        // Copied reference\n" +
+            "        changeApple(apple);\n" +
+            "        System.out.println(apple.color);\n" +
+            "    }\n" +
+            "\n" +
+            "    public static void changeApple(Apple apple) {\n" +
+            "        apple.color = \"green\";\n" +
+            "    }\n" +
+            "}";
+
     @GetMapping("/")
     public String viewHomePage(Model model) {
-        model.addAttribute("testdata", "This is the test data from Controller");
+        model.addAttribute("code", codeBlock);
         return "index";
     }
 }
